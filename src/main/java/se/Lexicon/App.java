8551381c3678e1db2320ff0b555724057be5c476
liljana ristevska <li.ristevska@gmail.com>
2022-10-24 15:00:41 +0200

    String ex2

---
+++ src/main/java/se/Lexicon/App.java
@@ -0,0 +1,15 @@
+package se.Lexicon;
+
+
+public class App {
+    public static void main(String[] args) {
+
+        String sen = "Long example sentence";
+
+        char ch = sen.charAt(6);
+        System.out.println(ch);
+
+
+    }
+
+}
