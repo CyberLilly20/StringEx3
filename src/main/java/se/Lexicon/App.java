c751061ceffc7b0b3ddd8d360e2d42b6ab34e319
liljana ristevska <li.ristevska@gmail.com>
2022-10-25 13:55:19 +0200

    String ex4

--- src/main/java/se/Lexicon/App.java
+++ src/main/java/se/Lexicon/App.java
@@ -3,9 +3,19 @@ package se.Lexicon;
 
 public class App {
     public static void main(String[] args) {
+        String sentence = "CAPS EQUALS SCREAMING";
+        System.out.println(sentence);
+        String upperCase ="CAPS EQUALS SCREAMING";
+        String toLowerCase = upperCase.toLowerCase();
+        System.out.println(toLowerCase);
+        String toUpperCase = toLowerCase.toLowerCase();
+        System.out.println(toLowerCase);
 
-        String sen = "Even longer example sentence";
-        System.out.println(sen.indexOf('o'));
+
+        System.out.println("Lower Case:" + " " + sentence.toLowerCase());
+        System.out.println("Upper Case:" + " " + sentence.toUpperCase());
+
+
     }
 
 }
---
+++ src/main/java/se/Lexicon/StringEx4.java
@@ -0,0 +1,19 @@
+package se.Lexicon;
+
+/**
+ * . Given the following String: "Ok this is not as long!"
+ * create a substring of only "not as long" (excluding the
+ * exclamation point) and print it out.
+ *
+ */
+
+public class StringEx4 {
+    public static void StringEx4 () {
+        String sen4 = "Ok this is not as long!";
+        System.out.println("Substring :"+" "+ sen4.substring(11,22));
+
+
+
+    }
+
+}
